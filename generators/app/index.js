6c8fc16968ababad7e76435961a14527d4b30f88
joelbandi <joelvivekbandi@gmail.com>
2017-05-15 02:56:49 -0700

    Second commit milestone

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,13 +2,47 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const Promise = require('bluebird');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
+  initializing() {
     this.log(yosay(
-      'Welcome to the Joel\'s ' + chalk.red('generator-nrrb') + ' generator!'
+      'Welcome to the Joel\'s ' + chalk.bold.cyan('nrrb') + ' generator!'
     ));
   }
-
+  prompting() {
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'project_name',
+        message: 'Your project name',
+        default: this.appname
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Your project\'s description',
+        default: '{your-app\'s-description}'
+      },
+      {
+        type: 'input',
+        name: 'github_username',
+        message: 'Your github username',
+        default: '{github-user-name}',
+        store: true
+      },
+      {
+        type: 'confirm',
+        name: 'use-yarn',
+        message: 'Do you want to use yarn?',
+        default: false,
+        store: true
+      }
+    ]).then((answers) => {
+      this.answers = answers;
+    });
+  }
+  install() {
+    this.log(this.answers);
+  }
 };
